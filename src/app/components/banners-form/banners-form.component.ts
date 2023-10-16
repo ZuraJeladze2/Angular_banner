263ec7f29dfadfaa3845ab1da2a39253688ea980
ZuraJeladze2 <zurajeladze2@gmail.com>
2023-10-16 21:11:53 +0400

    feature/formUpdate
    zone and label works as options. added blobFind

--- src/app/components/banners-form/banners-form.component.ts
+++ src/app/components/banners-form/banners-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DrawerComponent } from '../drawer/drawer.component';
+import { Subscription, catchError, map } from 'rxjs';
+import { BannerService } from '../../services/banner-service.service';
 
 @Component({
   selector: 'app-banners-form',
@@ -6,6 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./banners-form.component.css'],
 })
 export class BannersFormComponent {
+
+
+  constructor(private http: BannerService){ }
+  
   //image, title, zone, active, dates, labels
   formValues = {
     title: '',
@@ -14,6 +21,67 @@ export class BannersFormComponent {
     startDate: '',
     endDate: '',
     label: ''
-
   }
+  
+  blobRB = {  //find Request Body
+    blobIds: ['2'],
+  };
+  refRB = {
+    includes: [],
+    search: '', // Replace with your search query
+    sortBy: '',
+    // typeIds: null,
+    sortDirection: '',
+    pageIndex: 0,
+    pageSize: 100,
+  }
+
+  options = {
+    zone: [],
+    label: []
+  }
+
+  logForm(){
+    console.log(this.formValues);
+  }
+
+  getBlobs(){
+    this.http.getBlobsData(this.blobRB)
+    .pipe(
+      map(response => {
+        return response; // You might want to return the response
+      }),
+      catchError(error => {
+        console.error('API Error:', error);
+        throw error;
+      })
+    )
+    .subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  getRef(typeId: string, opts: string[]) {
+    this.refRB['typeId'] = typeId;
+    this.http.getRefData(this.refRB).pipe(
+      map(response => {
+        console.log(response);
+        return response.data.entities.map(item => item.name);
+      }),
+      catchError(error => {
+        console.log(error);
+        throw error;
+      })
+    ).subscribe(data => {
+      console.log(data);
+      data.forEach((el) => {
+        opts.push(el);
+      });
+    });
+  }
+  
+    ngOnInit(): void {
+      this.getRef('1700', this.options.zone)
+      this.getRef('1900', this.options.label)
+    }
 }
